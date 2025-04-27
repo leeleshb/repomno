8b818cc34510e2fd3283e124d86b92bf223e3ee4
bruce <bruce@test1.com>
2025-04-27 19:15:08 +0530

    second commit

--- src/test/java/demo/Sample.java
+++ src/test/java/demo/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("hi, i am working as an automation engineer");
+		System.out.println("hello evening");
 	}
 
 }
